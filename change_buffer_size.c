c358e4926e15d658fe05a50be87bbc81a1faa69c
choi4624 <choi4624@gmail.com>
2022-11-12 00:08:22 +0900

    fix change buffer size_working

--- change_buffer_size.c
+++ change_buffer_size.c
@@ -9,24 +9,30 @@
 #define CH_WRITE_BUFFER_SIZE _IOW(TEST_DEV_MAJOR,1,int)
 #define CH_READ_BUFFER_SIZE _IOW(TEST_DEV_MAJOR,2,int)
 
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
 
     int device;
+    int size = atoi(argv[2]);
+    int type = atoi(argv[1]);
     device = open("/dev/VirtualDisk", O_RDWR | O_NDELAY);
-    int size = atoi(argv[1]);
-    if (argc == 1)
+    printf("argc %d\n",argc);
+    printf("argv %c\n",*argv[1]);
+    printf("argv2 %d\n",size);
+    
+    if (type == 1)
     {
         ioctl(device, CH_WRITE_BUFFER_SIZE, &size);
+        printf("buffer wr changed %d\n", size);
     }
-    if (argc == 2)
+    if (type == 2)
     {
         ioctl(device, CH_READ_BUFFER_SIZE, &size);
+        printf("buffer rd changed %d\n", size);
     }
-    printf("buffer changed\n");
+    printf("changed\n");
 
     close(device);
-
 
     return 0;
 }
