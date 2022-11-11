6db404c53c64380e3b25cb8995e9cc0cf6fff081
choi4624 <choi4624@gmail.com>
2022-11-11 21:41:47 +0900

    first working

--- change_buffer_size.c
+++ change_buffer_size.c
@@ -1,7 +1,3 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/list.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,21 +5,20 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 
-#define TEST_DEV_NAME "VirtualDisk"
-#define TEST_DEV_MAJOR 246
+#define TEST_DEV_MAJOR 200
 
-#define CH_WRITE_BUFFER_SIZE _IOW(DEV_MAJOR_NUMBER,1,int)
-#define CH_READ_BUFFER_SIZE _IOW(DEV_MAJOR_NUMBER,2,int)
+#define CH_WRITE_BUFFER_SIZE _IOW(TEST_DEV_MAJOR,1,int)
+#define CH_READ_BUFFER_SIZE _IOW(TEST_DEV_MAJOR,2,int)
 
 int main(int argc, char **argv)
 {
     if (change_buffer_size == 1)
     {
-        read(&argc,&argv);
+        read(argc,argv);
     }
     if(change_buffer_size == 2)
     {
-        write(&argc,&argv);
+        write(argc,argv);
     }
 
     return 0;
@@ -34,7 +29,7 @@ int main(int argc, char **argv)
 int read(int argc, char **argv)
 {
     int device;
-    device = open("/dev/BufferedMem", O_RDWR | O_NDELAY)
+    device = open("/dev/VirtualDisk", O_RDWR | O_NDELAY);
     int size = atoi(argv[1]);
     ioctl(device, CH_READ_BUFFER_SIZE, &read_buffer_size);
     printf("buffer changed\n");
@@ -49,7 +44,7 @@ int read(int argc, char **argv)
 int write(int argc, char **argv)
 {
     int device;
-    device = open("/dev/BufferedMem", O_RDWR | O_NDELAY)
+    device = open("/dev/VirtualDisk", O_RDWR | O_NDELAY);
     int size = atoi(argv[1]);
     ioctl(device, CH_WRITE_BUFFER_SIZE, &buffer_size);
     printf("buffer changed\n");
--- virtual_disk_dd.c
+++ virtual_disk_dd.c
@@ -7,7 +7,7 @@
 #include <linux/uaccess.h>
 
 #define TEST_DEV_NAME "VirtualDisk"
-#define TEST_DEV_MAJOR 246
+#define TEST_DEV_MAJOR 266
 #define BUFFER_SIZE 128
 
 
@@ -19,19 +19,19 @@ module_param(read_buffer_size, int, 0000);
 
 struct kfifo fifo_buffer; // 내부 버퍼
 
-#define CH_WRITE_BUFFER_SIZE _IOW(DEV_MAJOR_NUMBER,1,int)
-#define CH_READ_BUFFER_SIZE _IOW(DEV_MAJOR_NUMBER,2,int)
+#define CH_WRITE_BUFFER_SIZE _IOW(TEST_DEV_MAJOR,1,int)
+#define CH_READ_BUFFER_SIZE _IOW(TEST_DEV_MAJOR,2,int)
 
 
-int device_open(struct inode *inode, struct file *filep)
+static int chr_open(struct inode *inode, struct file *filep)
 {
     int number = MINOR(inode->i_rdev);
-    pritnk("[CHR_DEV] Virtual Character Device open : Minor Number: %d\n", number);
+    printk("[CHR_DEV] Virtual Character Device open : Minor Number: %d\n", number);
 
     return 0;
 }
 
-ssize_t chr_write(struct file *filep, const char *buf, size_t count, loff_t *f_ops)
+static ssize_t chr_write(struct file *filep, const char *buf, size_t count, loff_t *f_ops)
 {
     unsigned int val;
     char * buffer;
@@ -49,18 +49,18 @@ ssize_t chr_write(struct file *filep, const char *buf, size_t count, loff_t *f_o
         if(kfifo_is_full(&fifo_buffer)){
             kfifo_out(&fifo_buffer, &letter, sizeof(letter));
         }
-        letter=write_buffer[i]; 
+        letter=buffer[i]; 
         kfifo_in(&fifo_buffer,&letter,sizeof(letter));  
         printk(KERN_INFO "%c", letter);
     }
     printk(KERN_INFO "\n", letter);
 
-    pritnk("[CHR_DEV] write data: %s\n", buffer);
+    printk("[CHR_DEV] write data: %s\n", buffer);
     printk(KERN_INFO "kfifo_in ok\n");
     return count;
 }
 
-ssize_t chr_read(struct file *filep, const char *buf, size_t count, loff_t *f_ops)
+static ssize_t chr_read(struct file *filep, char *buf, size_t count, loff_t *f_ops)
 {
     unsigned int val;
     char * buffer;
@@ -76,20 +76,20 @@ ssize_t chr_read(struct file *filep, const char *buf, size_t count, loff_t *f_op
     printk(KERN_INFO "reuslt\n");
     for ( i = 0; i < read_buffer_size; i++)
     {
-        if (!kfifo_is_empty(&fifo))
+        if (!kfifo_is_empty(&fifo_buffer))
         {
-        ret = kfifo_out(&fifo, &letter, sizeof(letter));
+        ret = kfifo_out(&fifo_buffer, &letter, sizeof(letter));
         buffer[i] = letter;
         printk(KERN_INFO "%c\n", letter);
         }    
     }
     
-    printk(KERN_INFO "kfifo size: %d\n", kfifo_size(&fifo));
-    printk(KERN_INFO "kfifo len: %d\n", kfifo_len(&fifo));
-    printk(KERN_INFO "kfifo avail: %d\n", kfifo_avail(&fifo));
+    printk(KERN_INFO "kfifo size: %d\n", kfifo_size(&fifo_buffer));
+    printk(KERN_INFO "kfifo len: %d\n", kfifo_len(&fifo_buffer));
+    printk(KERN_INFO "kfifo avail: %d\n", kfifo_avail(&fifo_buffer));
     
     val = read_buffer_size;
-    copy_to_user(buf,buffer,read_buffer_size)
+    copy_to_user(buf,buffer,read_buffer_size);
 
     printk(KERN_INFO "read ok\n");
 
@@ -98,7 +98,7 @@ ssize_t chr_read(struct file *filep, const char *buf, size_t count, loff_t *f_op
 
 // device 2 ES-10-device%20driver.pdf
 
-long chr_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
+static long chr_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 {
     int size;
     struct kfifo temp;
@@ -132,21 +132,20 @@ long chr_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
         kfifo_free(&temp);
 
         size = kfifo_size(&fifo_buffer);
-        printk(KERN_INFO "kfifo size: %d\n", kfifo_size(&fifo));
-        printk(KERN_INFO "kfifo len: %d\n", kfifo_len(&fifo));
-        printk(KERN_INFO "kfifo avail: %d\n", kfifo_avail(&fifo));
-        copy_to_user((void __user *)arg, &size, sizeof(size))
+        printk(KERN_INFO "kfifo size: %d\n", kfifo_size(&fifo_buffer));
+        printk(KERN_INFO "kfifo len: %d\n", kfifo_len(&fifo_buffer));
+        printk(KERN_INFO "kfifo avail: %d\n", kfifo_avail(&fifo_buffer));
+        copy_to_user((void __user *)arg, &size, sizeof(size));
         break;
 
     case CH_READ_BUFFER_SIZE:
         read_buffer_size = size;
         printk("[CHR_DEV] cmd value is %d\n", cmd);
-        copy_from_user(&size,(void __user *)arg,sizeof(size))
+        copy_from_user(&size,(void __user *)arg,sizeof(size));
         printk(KERN_INFO "read buffer size changed : %d \n", read_buffer_size);
         break;
 
     default:
-        read_buffer_size
         printk("[CHR_DEV] cmd value is %d default route\n", cmd);
         break;
     }
@@ -154,16 +153,16 @@ long chr_ioctl(struct file *filep, unsigned int cmd, unsigned long arg)
 
 }
 
-int chr_release(struct inode *inode, struct file *filep)
+static  int chr_release(struct inode *inode, struct file *filep)
 {
     printk("[CHR_DEV] Virtual Char Device Release\n");
     return 0; 
 }
 
 
-struct file_operations chr_fops =
+static struct file_operations chr_fops =
 {
-    .owner = THIS_MOULE,
+    .owner = THIS_MODULE,
     .write = chr_write,
     .read = chr_read,
     .open = chr_open, // NOT USED
@@ -174,10 +173,10 @@ struct file_operations chr_fops =
 
 
 
-int chr_init(void)
+static int chr_init(void)
 {
     // 장치 등록 
-    int registration, student;
+    int student;
     student = 201714038;
     printk(KERN_INFO "virutalDisk %d device driver registerd \n", student); 
 
@@ -187,25 +186,22 @@ int chr_init(void)
     }
 
     printk("[CHR_DEV] Registration Virtual Character Device to Kernel\n");
-    registration = register_chrdev(CHR_DEV_MAJOR, CHR_DEV_NAME, &chr_fops);
-    if (registration < 0)
-    {
-        return registration;
-    }
-    printk("[CHR_DEV] Major number : %d buffer size : %d\n", registration,kfifo_size(&fifo_buffer));
+    register_chrdev(TEST_DEV_MAJOR, TEST_DEV_NAME, &chr_fops);
+
+    printk("[CHR_DEV] Major number : [hallow] buffer size : %d\n", kfifo_size(&fifo_buffer));
 
     return 0;
     
 }
 
-void chr_exit(void)
+static void chr_exit(void)
 {
     int student = 201714038;
     printk("[CHR_DEV] UnRegistration Virtual Character Device from Kernel\n");
     // kfifo 해제, 장치 해제 
 
-    kfifo_free(&fifo);
-    unregister_chrdev(CHR_DEV_MAJOR, CHR_DEV_NAME);
+    kfifo_free(&fifo_buffer);
+    unregister_chrdev(TEST_DEV_MAJOR, TEST_DEV_NAME);
     printk(KERN_INFO "virutalDisk %d device driver unregisterd \n", student); 
 }
 
